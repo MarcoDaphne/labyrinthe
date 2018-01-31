96f59052f85eceaf2a756499e6aa14bc3ba679da
MarcoDaphne <marcodaphne@gmail.com>
2018-01-31 12:02:45 +0100

    Renommage structure par schema et modifications pour lisibilité

--- src/maze.py
+++ src/maze.py
@@ -7,7 +7,7 @@ import constant as c
 
 class Maze:
 	def __init__(self):
-		self.structure = []
+		self.schema = []
 		self.free_position = []
 		self.macgyver_location = ()
 		self.end_location = []
@@ -20,23 +20,23 @@ class Maze:
 			for i, line in enumerate(f):
 				clean_line = line.strip()
 				liste = list(clean_line)
-				self.structure.append(liste)
+				self.schema.append(liste)
 				for j, element in enumerate(liste):
 					if element == c.FREE:
 						self.free_position.append((i, j))
 					elif element == c.MACGYVER:
 						self.macgyver_location = i, j
 					elif element == c.GATEKEEPER:
-						self.end_location.append(((i-1, j), (i+1, j, i, j-1, i, j+1)))
-		return self.structure, self.free_position, self.macgyver_location, self.end_location
+						self.end_location.append(((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)))
+		return self.schema, self.free_position, self.macgyver_location, self.end_location
 
 	def get(self, i, j):
 		"""Gets a position in the maze"""
-		return self.structure[i][j]
+		return self.schema[i][j]
 
 	def set(self, i, j, element):
 		"""Install an element on a maze position"""
-		self.structure[i][j] = element
+		self.schema[i][j] = element
 
 	def locate_macgyver(self, i, j):
 		"""locate MacGyver after each move"""
@@ -44,7 +44,7 @@ class Maze:
 
 	def show(self):
 		"""Displays the maze"""
-		for line in self.structure:
+		for line in self.schema:
 			print(line)
 
 
