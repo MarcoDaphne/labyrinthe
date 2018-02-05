ffe2b3eb2a029957b4a87c1b49664772877453ed
MarcoDaphne <marcodaphne@gmail.com>
2018-02-05 10:30:24 +0100

    Code fonctionnel

--- src/syringe.py
+++ src/syringe.py
@@ -11,23 +11,22 @@ class Syringe:
 		self.mz = mz
 		self.items = [c.NEEDLE, c.TUBE, c.ETHER]
 		self.chosen_positions = []
-		self.items_positions = []
 
 	def find_random_position(self):
 		"""Return a random position"""
 		structure = self.mz.structure
 		passage = self.mz.free_positions
-		end_positions = self.mz.end_locations
+		end_positions = self.mz.end_locations[0]
 		i = -1
 		j = -1
 		while (i, j) not in passage:
-			i = random.randint(0, len(structure))
-			j = random.randint(0, len(structure[0]))
-			if (i, j) in passage:
-				return i, j
+			i = random.randint(0, len(structure) - 1)
+			j = random.randint(0, len(structure[0]) - 1)
+		else:
+			return i, j
 
 	def place_items(self):
-		"""Place three objects randomly on the maze""" 
+		"""Place three objects randomly on the maze"""
 		for element in self.items:
 			i, j = self.find_random_position()
 			self.mz.set(i, j, element)
