4d4ecb3d0428dbf5b65a864c7b08d5a9f276a90d
MarcoDaphne <marcodaphne@gmail.com>
2018-02-20 12:04:43 +0100

    Vérifications pep8 maze.py

--- src/maze.py
+++ src/maze.py
@@ -5,50 +5,52 @@ import os
 
 import constants as c
 
+
 class Maze:
-	def __init__(self):
-		self.structure = []
-		self.free_positions = []
-		self.macgyver_location = ()
-		self.end_locations = []
+    def __init__(self):
+        self.structure = []
+        self.free_pos = []
+        self.macgyver_location = ()
+        self.endl = []
 
-	def load(self, data="data", data_file="schema.txt"):
-		"""Loads a maze from a text file and retrieves useful positions""" 
-		directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-		path_to_file = os.path.join(directory, data, data_file)
-		with open(path_to_file) as f:
-			for i, line in enumerate(f):
-				clean_line = line.strip()
-				liste = list(clean_line)
-				self.structure.append(liste)
-				for j, element in enumerate(liste):
-					if element == c.FREE:
-						self.free_positions.append((i, j))
-					elif element == c.MACGYVER:
-						self.macgyver_location = i, j
-					elif element == c.GATEKEEPER:
-						self.end_locations.append(((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)))
-		return self.structure, self.free_positions, self.macgyver_location, self.end_locations
+    def load(self, data="data", data_file="schema.txt"):
+        """Loads a maze from a text file and retrieves useful positions"""
+        directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path_to_file = os.path.join(directory, data, data_file)
+        with open(path_to_file) as f:
+            for i, line in enumerate(f):
+                clean_line = line.strip()
+                liste = list(clean_line)
+                self.structure.append(liste)
+                for j, element in enumerate(liste):
+                    if element == c.FREE:
+                        self.free_pos.append((i, j))
+                    elif element == c.MACGYVER:
+                        self.macgyver_location = i, j
+                    elif element == c.GATEKEEPER:
+                        self.endl.append(
+                            ((i-1, j), (i+1, j), (i, j-1), (i, j+1)))
+        return self.structure, self.free_pos, self.macgyver_location, self.endl
 
-	def get(self, i, j):
-		"""Gets a position in the maze"""
-		return self.structure[i][j]
+    def get(self, i, j):
+        """Gets a position in the maze"""
+        return self.structure[i][j]
 
-	def set(self, i, j, element):
-		"""Install an element on a maze position"""
-		self.structure[i][j] = element
+    def set(self, i, j, element):
+        """Install an element on a maze position"""
+        self.structure[i][j] = element
 
-	def locate_macgyver(self, i, j):
-		"""locate MacGyver after each move"""
-		self.macgyver_location = i, j
+    def locate_macgyver(self, i, j):
+        """locate MacGyver after each move"""
+        self.macgyver_location = i, j
 
-	def show(self):
-		"""Displays the maze"""
-		for line in self.structure:
-			print(line)
+    def show(self):
+        """Displays the maze"""
+        for line in self.structure:
+            print(line)
 
 
 if __name__ == "__main__":
-	maze = Maze()
-	maze.load()
-	maze.show()
+    maze = Maze()
+    maze.load()
+    maze.show()
