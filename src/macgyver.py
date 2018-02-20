0fd9694be28e46dc2f4ce95841a615072eb2a5d1
MarcoDaphne <marcodaphne@gmail.com>
2018-02-20 16:18:59 +0100

    Vérification PEP8 sur macgyver.py et syringe.py

--- src/macgyver.py
+++ src/macgyver.py
@@ -5,81 +5,90 @@ import maze as mz
 import syringe as sy
 import constants as c
 
+
 class Macgyver:
-	def __init__(self, mz, sy):
-		self.mz = mz
-		self.sy = sy
-		self.bag = []
+    def __init__(self, mz, sy):
+        self.mz = mz
+        self.sy = sy
+        self.bag = []
 
-	def move_and_locate(self, i, j):
-		"""Move then locate MacGyver sur le labyrinthe"""
-		self.mz.set(i, j, c.MACGYVER)
-		self.mz.locate_macgyver(i, j)
+    def move_and_locate(self, i, j):
+        """Move then locate MacGyver sur le labyrinthe"""
+        self.mz.set(i, j, c.MACGYVER)
+        self.mz.locate_macgyver(i, j)
 
-	def pick_up_or_move(self, i, j):
-		"""Retrieves objects if existing or moves"""
-		if self.mz.get(i, j) == c.NEEDLE:
-			self.bag.append(c.NEEDLE)
-			self.move_and_locate(i, j)
-		elif self.mz.get(i, j) == c.TUBE:
-			self.bag.append(c.TUBE)
-			self.move_and_locate(i, j)
-		elif self.mz.get(i, j) == c.ETHER:
-			self.bag.append(c.ETHER)
-			self.move_and_locate(i, j)
-		elif len(self.bag) == len(self.sy.items) and (i, j) in self.mz.end_locations[0]:
-			self.move_and_locate(i, j)
-			#self.mz.show()
-			print("==> Bravo!!! Vous avez endormi Murdoc!!!")
-			return c.WIN
-		elif (i, j) in self.mz.end_locations[0]:
-			self.move_and_locate(i, j)
-			#self.mz.show()
-			print("==> Murdoc a tué MacGyver...")
-			return c.LOOSE
-		else:	
-			self.move_and_locate(i, j)
+    def pick_up_or_move(self, i, j):
+        """Retrieves objects if existing or moves"""
+        if self.mz.get(i, j) == c.NEEDLE:
+            self.bag.append(c.NEEDLE)
+            self.move_and_locate(i, j)
+        elif self.mz.get(i, j) == c.TUBE:
+            self.bag.append(c.TUBE)
+            self.move_and_locate(i, j)
+        elif self.mz.get(i, j) == c.ETHER:
+            self.bag.append(c.ETHER)
+            self.move_and_locate(i, j)
+        elif len(self.bag) == len(self.sy.items) and (i, j) in self.mz.endl[0]:
+            self.move_and_locate(i, j)
+            # self.mz.show()
+            print("==> Bravo!!! Vous avez endormi Murdoc!!!")
+            return c.WIN
+        elif (i, j) in self.mz.endl[0]:
+            self.move_and_locate(i, j)
+            # self.mz.show()
+            print("==> Murdoc a tué MacGyver...")
+            return c.LOOSE
+        else:
+            self.move_and_locate(i, j)
 
-	def step_up(self):
-		"""Move Macgyver one position up"""
-		structure = self.mz.structure
-		i, j = self.mz.macgyver_location
-		if self.mz.get(i - 1, j) != c.WALL and (i, j) not in self.mz.end_locations[0] and 0 < i < len(structure):
-			self.mz.set(i, j, c.FREE)
-			return self.pick_up_or_move(i - 1, j)
-	
-	def step_right(self):
-		"""Move Macgyver one position to the right"""
-		structure = self.mz.structure
-		i, j = self.mz.macgyver_location
-		if self.mz.get(i, j + 1) != c.WALL and (i, j) not in self.mz.end_locations[0] and 0 < j < len(structure):
-			self.mz.set(i, j, c.FREE)
-			return self.pick_up_or_move(i, j + 1)
+    def step_up(self):
+        """Move Macgyver one position up"""
+        i, j = self.mz.macgyver_location
+        strt = self.mz.structure
+        get = self.mz.get(i - 1, j)
+        endl = self.mz.endl[0]
+        if get != c.WALL and (i, j) not in endl and 0 < i < len(strt):
+            self.mz.set(i, j, c.FREE)
+            return self.pick_up_or_move(i - 1, j)
 
-	def step_down(self):
-		"""Move Macgyver one position down"""
-		structure = self.mz.structure
-		i, j = self.mz.macgyver_location
-		if self.mz.get(i + 1, j) != c.WALL and (i, j) not in self.mz.end_locations[0] and 0 <= i < len(structure):
-			self.mz.set(i, j, c.FREE)
-			return self.pick_up_or_move(i + 1, j)
+    def step_right(self):
+        """Move Macgyver one position to the right"""
+        i, j = self.mz.macgyver_location
+        strt = self.mz.structure
+        get = self.mz.get(i, j + 1)
+        endl = self.mz.endl[0]
+        if get != c.WALL and (i, j) not in endl and 0 < j < len(strt):
+            self.mz.set(i, j, c.FREE)
+            return self.pick_up_or_move(i, j + 1)
 
-	def step_left(self):
-		"""Move Macgyver one position to the left"""
-		structure = self.mz.structure
-		i, j = self.mz.macgyver_location
-		if self.mz.get(i, j - 1) != c.WALL and (i, j) not in self.mz.end_locations[0] and 0 < j < len(structure):
-			self.mz.set(i, j, c.FREE)
-			return self.pick_up_or_move(i, j - 1)
+    def step_down(self):
+        """Move Macgyver one position down"""
+        i, j = self.mz.macgyver_location
+        strt = self.mz.structure
+        get = self.mz.get(i + 1, j)
+        endl = self.mz.endl[0]
+        if get != c.WALL and (i, j) not in endl and 0 <= i < len(strt):
+            self.mz.set(i, j, c.FREE)
+            return self.pick_up_or_move(i + 1, j)
+
+    def step_left(self):
+        """Move Macgyver one position to the left"""
+        i, j = self.mz.macgyver_location
+        strt = self.mz.structure
+        get = self.mz.get(i, j - 1)
+        endl = self.mz.endl[0]
+        if get != c.WALL and (i, j) not in endl and 0 < j < len(strt):
+            self.mz.set(i, j, c.FREE)
+            return self.pick_up_or_move(i, j - 1)
 
 
 if __name__ == "__main__":
-	maze = mz.Maze()
-	maze.load()
-	syringe = sy.Syringe(maze)
-	syringe.place_items()
-	macgyver = Macgyver(maze, syringe)
-	macgyver.step_up()
-	macgyver.step_right()
-	macgyver.step_down()
-	macgyver.step_left()
+    maze = mz.Maze()
+    maze.load()
+    syringe = sy.Syringe(maze)
+    syringe.place_items()
+    macgyver = Macgyver(maze, syringe)
+    macgyver.step_up()
+    macgyver.step_right()
+    macgyver.step_down()
+    macgyver.step_left()
--- src/syringe.py
+++ src/syringe.py
@@ -6,31 +6,32 @@ import random
 import maze as mz
 import constants as c
 
+
 class Syringe:
-	def __init__(self, mz):
-		self.mz = mz
-		self.items = [c.NEEDLE, c.TUBE, c.ETHER]
-		self.chosen_positions = []
+    def __init__(self, mz):
+        self.mz = mz
+        self.items = [c.NEEDLE, c.TUBE, c.ETHER]
+        self.chosen_positions = []
 
-	def find_random_positions(self):
-		"""Return a random position"""
-		passage = set(self.mz.free_positions)
-		end_positions = set(self.mz.end_locations[0])
-		freeway = passage - end_positions
-		chosen_positions = random.sample(freeway, len(self.items))
-		for position in chosen_positions:
-			return position
+    def find_random_positions(self):
+        """Return a random position"""
+        passage = set(self.mz.free_pos)
+        end_positions = set(self.mz.endl[0])
+        freeway = passage - end_positions
+        chosen_positions = random.sample(freeway, len(self.items))
+        for position in chosen_positions:
+            return position
 
-	def place_items(self):
-		"""Place three objects randomly on the maze"""
-		for item in self.items:
-			i, j = self.find_random_positions()
-			self.mz.set(i, j, item)
+    def place_items(self):
+        """Place three objects randomly on the maze"""
+        for item in self.items:
+            i, j = self.find_random_positions()
+            self.mz.set(i, j, item)
 
 
 if __name__ == "__main__":
-	maze = mz.Maze()
-	syringe = Syringe(maze)
-	maze.load()
-	syringe.place_items()
-	maze.show()
+    maze = mz.Maze()
+    syringe = Syringe(maze)
+    maze.load()
+    syringe.place_items()
+    maze.show()
